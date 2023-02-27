fixed scaling on rev rxns + consistent naming

bondnet/data/dataset.py
@@ -797,7 +797,6 @@ class ReactionNetworkDatasetGraphs(BaseDataset):
 
         # feature transformers
         if self.feature_transformer:
-
             if self.state_dict_filename is None:
                 feature_scaler = HeteroGraphFeatureStandardScaler(mean=None, std=None)
             else:
@@ -867,9 +866,9 @@ class ReactionNetworkDatasetGraphs(BaseDataset):
                     lab_temp = torch.zeros(self.classif_categories)
                     lab_temp[int(lb['value'][0])] = 1
 
-                    if(lb['rev_value']!= None):
+                    if(lb['value_rev']!= None):
                         lab_temp_rev = torch.zeros(self.classif_categories)
-                        lab_temp[int(lb['rev_value'][0])] = 1
+                        lab_temp[int(lb['value_rev'][0])] = 1
                     else: lab_temp_rev = None
                         
                     label = {
@@ -890,7 +889,7 @@ class ReactionNetworkDatasetGraphs(BaseDataset):
                             lb["value"], dtype=getattr(torch, self.dtype)
                         ),
                         "value_rev":  torch.tensor(
-                            lb["rev_value"], dtype=getattr(torch, self.dtype)
+                            lb["value_rev"], dtype=getattr(torch, self.dtype)
                         ),
                         "id": lb["id"],
                         "environment": environemnt,
@@ -914,6 +913,7 @@ class ReactionNetworkDatasetGraphs(BaseDataset):
 
             # normalization
             values = torch.stack([lb["value"] for lb in self.labels])  # 1D tensor
+            values_rev = torch.stack([lb["value_rev"] for lb in self.labels])
 
             if self.state_dict_filename is None:
                 mean = torch.mean(values)
@@ -931,12 +931,14 @@ class ReactionNetworkDatasetGraphs(BaseDataset):
                 std = self._label_scaler_std
 
             values = (values - mean) / std
+            value_rev_scaled = (values_rev - mean) / std
 
             # update label
             for i, lb in enumerate(values):
                 self.labels[i]["value"] = lb
                 self.labels[i]["scaler_mean"] = mean
                 self.labels[i]["scaler_stdev"] = std
+                self.labels[i]["value_rev"] = value_rev_scaled[i]
 
             logger.info(f"Label scaler mean: {mean}")
             logger.info(f"Label scaler std: {std}")

bondnet/model/gated_reaction_network_lightning.py
@@ -19,6 +19,7 @@ from bondnet.model.metric import (
     Metrics_WeightedMSE
 )
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 
@@ -203,8 +204,6 @@ class GatedGCNReactionNetworkLightning(pl.LightningModule):
         self.fc_layers.append(nn.Linear(in_size, outdim))
         self.loss = self.loss_function()
         
-        #self.MSE = WeightedMSELoss(reduction="mean")
-        #self.L1 = WeightedL1Loss(reduction="mean")
         self.train_l1 = Metrics_WeightedMAE()
         self.train_mse = Metrics_WeightedMSE()
         self.train_r2 = torchmetrics.R2Score()
@@ -217,7 +216,6 @@ class GatedGCNReactionNetworkLightning(pl.LightningModule):
         self.test_mse = Metrics_WeightedMSE()
         self.test_r2 = torchmetrics.R2Score()
         
-
 
     def forward(self, graph, feats, reactions, norm_atom=None, norm_bond=None, device=None, reverse = False):
         """
@@ -307,6 +305,52 @@ class GatedGCNReactionNetworkLightning(pl.LightningModule):
         return all_feats
 
 
+    def shared_step(self, batch, mode):
+
+        # ========== compute predictions ==========
+        batched_graph, label = batch
+        nodes = ["atom", "bond", "global"]
+        feats = {nt: batched_graph.nodes[nt].data["feat"] for nt in nodes}
+        target = label["value"].view(-1)
+        target_aug = label["value_rev"].view(-1)       
+        empty_aug = torch.isnan(target_aug).tolist()
+        empty_aug = True in empty_aug
+        norm_atom = label["norm_atom"]
+        norm_bond = label["norm_bond"]
+        
+        stdev = label["scaler_stdev"]
+        if self.device is not None:
+            feats = {k: v.to(self.device) for k, v in feats.items()}
+            target = target.to(self.device)
+            norm_atom = norm_atom.to(self.device)
+            norm_bond = norm_bond.to(self.device)   
+            stdev = stdev.to(self.device)
+            #if(self.augment and not empty_aug): 
+            #    target_aug = target_aug.to(self.device)
+
+        pred = self(
+            batched_graph, 
+            feats, 
+            label["reaction"], 
+            device=self.device, 
+            norm_bond = norm_bond, 
+            norm_atom=norm_atom)
+        
+        pred = pred.view(-1)
+
+        #feats, reaction_feats = self(mol_graphs, rxn_graphs, feats, metadata)
+        #preds = self.decode(feats, reaction_feats, metadata)
+
+        # ========== compute losses ==========
+        all_loss = self.compute_loss(pred, target, stdev)
+        # ========== logger the loss ==========
+
+        self.log(f"{mode}_loss", all_loss, on_step=False, on_epoch=True, prog_bar=True, batch_size=len(label))
+        self.update_metrics(target, pred, stdev, mode)
+
+        return all_loss
+
+    
     def loss_function(self): 
         """
         Initialize loss function
@@ -390,41 +434,6 @@ class GatedGCNReactionNetworkLightning(pl.LightningModule):
         """
         Test step
         """
-        return self.shared_step(batch, mode="test")
-
-
-    def training_epoch_end(self, outputs):
-        """
-        Training epoch end
-        """
-        l1, rmse, r2 =  self.compute_metrics(mode = "train")
-        self.log("train_l1", l1, prog_bar=True)
-        self.log("train_rmse", rmse)
-        self.log("train_r2", r2, prog_bar=True)
-
-
-    def validation_epoch_end(self, outputs):
-        """
-        Validation epoch end
-        """
-        l1, rmse, r2 =  self.compute_metrics(mode = "val")
-        self.log("val_l1", l1, prog_bar=True)
-        self.log("val_rmse", rmse)
-        self.log("val_r2", r2, prog_bar=True)
-
-
-    def test_epoch_end(self, outputs):
-        """
-        Test epoch end
-        """
-        l1, rmse, r2 =  self.compute_metrics(mode = "test")
-        self.log("test_l1", l1, prog_bar=True)
-        self.log("test_rmse", rmse, prog_bar=True)
-        self.log("test_r2", r2, prog_bar=True)
-
-
-    def shared_step(self, batch, mode):
-
         # ========== compute predictions ==========
         batched_graph, label = batch
         nodes = ["atom", "bond", "global"]
@@ -456,19 +465,58 @@ class GatedGCNReactionNetworkLightning(pl.LightningModule):
         
         pred = pred.view(-1)
 
-        #feats, reaction_feats = self(mol_graphs, rxn_graphs, feats, metadata)
-        #preds = self.decode(feats, reaction_feats, metadata)
+        # convert bf16 to fp32
+        
+        pred = pred.to(torch.float32)
+        target = target.to(torch.float32)
+        stdev = stdev.to(torch.float32)
+        pred_np = pred.detach().cpu().numpy()
+        target_np = target.detach().cpu().numpy()  
+        stdev_np = stdev.detach().cpu().numpy()
+        plt.scatter(pred_np * stdev_np, target_np * stdev_np)
+        min_val = np.min([np.min(pred_np), np.min(target_np)]) - 0.5
+        max_val = np.max([np.max(pred_np), np.max(target_np)]) + 0.5
+        #plt.ylim(min_val,max_val)
+        #plt.xlim(min_val,max_val)
+        plt.title("Predicted vs. True")
+        plt.xlabel("Predicted")
+        plt.ylabel("True")
+        plt.savefig("./{}.png".format("./test"))
 
-        # ========== compute losses ==========
-        all_loss = self.compute_loss(pred, target, stdev)
-        # ========== logger the loss ==========
 
-        self.log(f"{mode}_loss", all_loss, on_step=False, on_epoch=True, prog_bar=True, batch_size=len(label))
-        self.update_metrics(target, pred, stdev, mode)
+        return self.shared_step(batch, mode="test")
 
-        return all_loss
 
-    
+    def training_epoch_end(self, outputs):
+        """
+        Training epoch end
+        """
+        l1, rmse, r2 =  self.compute_metrics(mode = "train")
+        self.log("train_l1", l1, prog_bar=True)
+        self.log("train_rmse", rmse, prog_bar=True)
+        self.log("train_r2", r2, prog_bar=True)
+
+
+    def validation_epoch_end(self, outputs):
+        """
+        Validation epoch end
+        """
+        l1, rmse, r2 =  self.compute_metrics(mode = "val")
+        self.log("val_l1", l1, prog_bar=True)
+        self.log("val_rmse", rmse)
+        self.log("val_r2", r2, prog_bar=True)
+
+
+    def test_epoch_end(self, outputs):
+        """
+        Test epoch end
+        """
+        l1, rmse, r2 =  self.compute_metrics(mode = "test")
+        self.log("test_l1", l1, prog_bar=True)
+        self.log("test_rmse", rmse, prog_bar=True)
+        self.log("test_r2", r2, prog_bar=True)
+
+
     def update_metrics(self, label, pred, weight, mode):
 
         if mode == 'train': 
@@ -480,6 +528,7 @@ class GatedGCNReactionNetworkLightning(pl.LightningModule):
             self.val_l1.update(pred, label, weight, reduction='sum')
             self.val_mse.update(pred, label, weight, reduction='sum')
             self.val_r2.update(pred, label) 
+        
         elif mode == 'test':
             self.test_l1.update(pred, label, weight, reduction='sum')
             self.test_mse.update(pred, label, weight, reduction='sum')
@@ -510,6 +559,7 @@ class GatedGCNReactionNetworkLightning(pl.LightningModule):
             self.test_r2.reset()
             self.test_mse.reset()
             self.test_l1.reset()
+        
         return l1, torch.sqrt(mse), r2
 
 

bondnet/model/metric.py
@@ -6,6 +6,8 @@ from torch import nn
 from torch.nn import functional as F
 from torch.nn import SmoothL1Loss
 from torchmetrics import Metric
+from torchmetrics import MeanSquaredError, MeanAbsoluteError
+
 from torch import Tensor, tensor
 
 class WeightedMSELoss(nn.Module):
@@ -109,6 +111,7 @@ class WeightedSmoothL1Loss(nn.Module):
 
     def forward(self, input, target, weight):
 
+        
         if weight is None:
             return F.smooth_l1_loss(input, target, reduction=self.reduction)
         else:
@@ -129,6 +132,93 @@ class WeightedSmoothL1Loss(nn.Module):
                     rst = torch.sum(rst)
 
             return rst
+
+
+class Metrics_WeightedMAE(Metric):
+    def __init__(self):
+        super().__init__()
+        # to count the correct predictions
+        is_differentiable: bool = True
+        higher_is_better: bool = False
+        full_state_update: bool = False
+        sum_abs_error: Tensor
+        total: Tensor
+
+        self.add_state("sum_abs_error", default=tensor(0.0), dist_reduce_fx="sum")
+        self.add_state("total", default=tensor(0), dist_reduce_fx="sum")
+
+
+    def update(self, preds: Tensor, target: Tensor, weight: Tensor, reduction: str = "sum")-> None:
+        
+
+        preds = preds if preds.is_floating_point else preds.float()
+        target = target if target.is_floating_point else target.float()
+        
+
+        if weight is None:
+            return F.l1_loss(preds, target, reduction=reduction)
+
+        preds = preds if preds.is_floating_point else preds.float()
+        target = target if target.is_floating_point else target.float()
+        sum_abs_error = torch.abs(preds - target) * weight
+        
+        if reduction != "none":
+            if reduction == "mean":
+                sum_abs_error = torch.sum(sum_abs_error) / torch.sum(weight)
+            else:
+                sum_abs_error = torch.sum(sum_abs_error)
+        
+        n_obs = target.numel()
+
+        self.sum_abs_error += sum_abs_error
+        self.total += n_obs
+
+    def compute(self):
+        return self.sum_abs_error / self.total
+    
+
+class Metrics_WeightedMSE(Metric):
+    def __init__(self):
+        super().__init__()
+        # to count the correct predictions
+        is_differentiable: bool = True
+        higher_is_better: bool = False
+        full_state_update: bool = False
+        sum_abs_error: Tensor
+        total: Tensor
+
+        self.add_state("sum_abs_error", default=tensor(0.0), dist_reduce_fx="sum")
+        self.add_state("total", default=tensor(0), dist_reduce_fx="sum")
+
+
+    def update(self, preds: Tensor, target: Tensor, weight: Tensor, reduction: str = "sum")-> None:
+
+
+        preds = preds if preds.is_floating_point else preds.float()
+        target = target if target.is_floating_point else target.float()
+        
+
+        if weight is None:
+            return F.mse_loss(preds, target, reduction=reduction)
+        
+        else:
+                    
+            sum_abs_error = ((preds - target) ** 2)
+            sum_abs_error *= weight
+            if reduction != "none":
+                if reduction == "mean":
+                    sum_abs_error = torch.sum(sum_abs_error) / torch.sum(weight)
+                else:
+                    sum_abs_error = torch.sum(sum_abs_error)
+
+        n_obs = target.numel()
+        self.sum_abs_error += sum_abs_error
+        self.total += n_obs
+
+    def compute(self):
+        return self.sum_abs_error / self.total
+    
+
 
 class OrderAccuracy:
     """
@@ -256,78 +346,3 @@ class ProgressMeter:
         fmt = "{:" + str(num_digits) + "d}"
         return "[" + fmt + "/" + fmt.format(num_batches) + "]"
 
-
-
-class Metrics_WeightedMAE(Metric):
-    def __init__(self):
-        super().__init__()
-        # to count the correct predictions
-        is_differentiable: bool = True
-        higher_is_better: bool = False
-        full_state_update: bool = False
-        sum_abs_error: Tensor
-        total: Tensor
-
-        self.add_state("sum_abs_error", default=tensor(0.0), dist_reduce_fx="sum")
-        self.add_state("total", default=tensor(0), dist_reduce_fx="sum")
-
-
-    def update(self, preds: Tensor, target: Tensor, weight: Tensor, reduction: str = "sum")-> None:
-        preds = preds if preds.is_floating_point else preds.float()
-        target = target if target.is_floating_point else target.float()
-        sum_abs_error = torch.abs(preds - target) * weight
-        if reduction != "none":
-            if reduction == "mean":
-                sum_abs_error = torch.sum(sum_abs_error) / torch.sum(weight)
-            else:
-                sum_abs_error = torch.sum(sum_abs_error)
-        n_obs = target.numel()
-
-
-        self.sum_abs_error += sum_abs_error
-        self.total += n_obs
-
-    def compute(self):
-        return self.sum_abs_error / self.total
-    
-
-class Metrics_WeightedMSE(Metric):
-    def __init__(self):
-        super().__init__()
-        # to count the correct predictions
-        is_differentiable: bool = True
-        higher_is_better: bool = False
-        full_state_update: bool = False
-        sum_abs_error: Tensor
-        total: Tensor
-
-        self.add_state("sum_abs_error", default=tensor(0.0), dist_reduce_fx="sum")
-        self.add_state("total", default=tensor(0), dist_reduce_fx="sum")
-
-
-    def update(self, preds: Tensor, target: Tensor, weight: Tensor, reduction: str = "sum")-> None:
-
-        preds = preds if preds.is_floating_point else preds.float()
-        target = target if target.is_floating_point else target.float()
-        
-        if weight is None:
-            return F.l1_loss(preds, target, reduction=reduction)
-        
-        else:
-                    
-            sum_abs_error = ((preds - target) ** 2)
-            sum_abs_error *= weight
-            if reduction != "none":
-                if reduction == "mean":
-                    sum_abs_error = torch.sum(sum_abs_error) / torch.sum(weight)
-                else:
-                    sum_abs_error = torch.sum(sum_abs_error)
-
-        n_obs = target.numel()
-
-
-        self.sum_abs_error += sum_abs_error
-        self.total += n_obs
-
-    def compute(self):
-        return self.sum_abs_error / self.total